6ba3fc607be05fd208ce7cb07d42aac43b8be484
wadjakorn <wadjakorn@dollarsign.co>
2018-03-28 22:22:18 +0700

    add helemt on App

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
@@ -7,6 +8,10 @@ import 'antd/dist/antd.css';
 export default function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>zxc-soft.com</title>
+      </Helmet>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
